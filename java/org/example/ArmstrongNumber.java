fa8f12b76444f599a365028787b9d6a3ea40db6a
Ahmet Kaya <146721631+Ahmetttttt7878@users.noreply.github.com>
2023-11-01 10:32:32 +0300

    Workshop 30.10.2023

---
+++ java/org/example/ArmstrongNumber.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueProgram = true;
+
+        while (continueProgram){
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+
+            int mainNumber = number;
+            int result = 0;
+            int n =0;
+
+            while (mainNumber !=0){
+                mainNumber /= 10;
+                n++;
+            }
+
+            mainNumber = number;
+
+            while (mainNumber !=0){
+                int digit = mainNumber % 10;
+                result += (int) Math.pow(digit,n);
+                mainNumber /= 10;
+            }
+            if (result == number){
+                System.out.println(number + " is an Armstrong number:!");
+            }else {
+                System.out.println(number + " is not an Amstrong number!");
+            }
+
+            System.out.print(" Press 'y' to continue, 'enter' to exit:");
+            String choice = scanner.next().toLowerCase();
+            if (!choice.equals("y")){
+                continueProgram = false;
+            }
+        }
+        scanner.close();
+    }
+    }
---
+++ java/org/example/PerfectNumber.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int count = 0; // The count of found perfect numbers
+        long number = 2; // The starting value of the number used in the formula
+
+        System.out.println("10 Perfect Numbers:");
+
+        while (count < 10) {
+            // Calculate the perfect number
+            long perfect = ((1L << number) - 1) * (1L << (number - 1));
+
+            if (isPerfect(perfect)) { // Check if it's a perfect number
+                System.out.println(perfect); // Print the perfect number
+                count++;
+            }
+            number++;
+        }
+    }
+
+    // A function to check whether a number is perfect
+    public static boolean isPerfect(long n) {
+        long sum = 1; // 1 is always used as a divisor
+        for (long i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i != n / i) {
+                    sum += n / i;
+                }
+            }
+        }
+        return sum == n; // Is the sum of divisors equal to the original number?
+    }
+}
